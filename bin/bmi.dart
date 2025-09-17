49d8a96db8a5261b9235be0456932d338192c6c3
karinaika <karinaikaindasa@gmail.com>
2025-09-17 18:04:56 +0700

    fitur bmi calculation

--- bin/bmi.dart
+++ bin/bmi.dart
@@ -13,4 +13,17 @@ void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riway
   }
 
   // (hitung BMI + kategori ditambahkan di branch selanjutnya)
+  double tinggiM = tinggiCm / 100;
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori="Obesitas";
+  }
 }
